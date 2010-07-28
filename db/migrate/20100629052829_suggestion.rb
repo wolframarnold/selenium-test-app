1731c48e1030ced5ea037e4826b39e24c5e678e9
Wolfram Arnold <wolfram@rubyfocus.biz>
2010-07-27 18:28:10 -0700

    remote :force option for migration due to Postgres/Heroku failure

--- db/migrate/20100629052829_suggestion.rb
+++ db/migrate/20100629052829_suggestion.rb
@@ -1,6 +1,6 @@
 class Suggestion < ActiveRecord::Migration
   def self.up
-    create_table :suggestions, :force => true do |t|
+    create_table :suggestions do |t|
       t.belongs_to :user, :null => false
       t.string :title, :null => false
       t.string :rating
